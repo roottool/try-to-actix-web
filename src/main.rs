a7dee21c119d427d11fe173111458ad86cdac70f
roottool <operation.versatile.roottool@gmail.com>
2022-09-25 03:30:33 +0000

    feat: Init axum

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use axum::{
+    // Json,
+    routing::get,
+    Router,
+};
+use std::net::SocketAddr;
+use tracing_subscriber::FmtSubscriber;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+#[tokio::main]
+async fn main() {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(tracing::Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    let app = Router::new().route("/", get(hello));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    tracing::info!("listening on {}", addr);
+
+    axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[tracing::instrument]
+async fn hello() -> &'static str {
+    tracing::info!("Access health check");
+    "Hello, World!"
 }
