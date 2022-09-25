5c0c4f11cdffcb8c9f253ef845e2b27507412d1d
roottool <operation.versatile.roottool@gmail.com>
2022-09-25 18:14:17 +0900

    Add a function which needs a query (#4)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use axum::{response::IntoResponse, routing::get, Json, Router};
-use serde::Serialize;
+use axum::{extract::Query, response::IntoResponse, routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 use std::net::SocketAddr;
 use tracing_subscriber::FmtSubscriber;
 
@@ -10,7 +11,9 @@ async fn main() {
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
-    let app = Router::new().route("/", get(hello));
+    let app = Router::new()
+        .route("/", get(hello))
+        .route("/hello", get(hello_with_query));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     tracing::info!("listening on {}", addr);
@@ -25,15 +28,31 @@ async fn main() {
 async fn hello() -> impl IntoResponse {
     tracing::info!("Access health check");
 
-    let hello_world = HelloWorld {
+    let hello_world = HelloWorldResponse {
         country: "Japan",
         hello_world: "ハローワールド",
     };
     Json(hello_world)
 }
 
+#[tracing::instrument]
+async fn hello_with_query(Query(params): Query<HelloWorldPayload>) -> Json<Value> {
+    tracing::info!("Access health check");
+
+    let hello_world = HelloWorldResponse {
+        country: &params.country,
+        hello_world: "Hello World",
+    };
+    Json(json!(hello_world))
+}
+
+#[derive(Deserialize, Debug)]
+struct HelloWorldPayload {
+    country: String,
+}
+
 #[derive(Serialize)]
-struct HelloWorld<'a> {
+struct HelloWorldResponse<'a> {
     country: &'a str,
     hello_world: &'a str,
 }
